031f81b935e78577c4229788da0b3f65444ecfbc
Dima <dmitya_karpov@mail.ru>
2025-08-04 19:45:51 +0300

    add timer

--- 1.py
+++ 1.py
@@ -1,9 +1,18 @@
+import time
+
 questions = {'Сколько планет в солнечной системе': '8', 'Столица франции':'Париж'}
 
 score = 0
 
 for q, a in questions.items():
+    start_time = time.time()
     user_ansver = input(q + ' ')
+    end_time = time.time()
+    if round(end_time - start_time, 1) > 5:
+        print('Вы неуложились в таймер 5 секунд!')
+        user_ansver = 'wrong'
+
+    print(f'Время ответа: {round(end_time - start_time, 1)} сек.')
     if user_ansver.lower() == a.lower():
         print('Вернор!')
         score +=1
